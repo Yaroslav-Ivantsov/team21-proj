e0b6fbe2627ae0be161c5f0093c1f48292fdf7c9
Yaroslav-Ivantsov <106278416+Yaroslav-Ivantsov@users.noreply.github.com>
2022-08-22 15:05:43 +0000

    Deploying to gh-pages from @ Yaroslav-Ivantsov/team21-proj@b351bef9b5f01a851c4c8de2b1882d2d0a8da011 🚀

--- index.db86b10b.js
+++
@@ -1,2 +0,0 @@
-function scrollToTop(){var o=this,l=$(".scroll-to-top");$(window).on("scroll",(function(){$(o).scrollTop()>=50?l.fadeIn():l.fadeOut()})),l.on("click",(function(o){o.preventDefault(),$("html").animate({scrollTop:0},1e3)}))}scrollToTop();
-//# sourceMappingURL=index.db86b10b.js.map
